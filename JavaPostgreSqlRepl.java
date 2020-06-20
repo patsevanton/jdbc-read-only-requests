1fb556f7aaf076387f645992cfa06b028a20b008
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-06-20 22:31:49 +0600

    Update JavaPostgreSqlRepl.java

--- JavaPostgreSqlRepl.java
+++ JavaPostgreSqlRepl.java
@@ -19,15 +19,15 @@ public class JavaPostgreSqlRepl {
     public static void main(String[] args) {
 
 	//jdbc:postgresql://node1,node2,node3/accounting?targetServerType=primary
-        String url = "jdbc:postgresql://localhost:5000/test?targetServerType=primary";
-        String user = "root";
-        String password = "";
+        String url = "jdbc:postgresql://localhost:5432/test?targetServerType=primary";
+        String user = "test";
+        String password = "password";
 
 	//jdbc:postgresql://node1,node2,node3/accounting?targetServerType=preferSecondary&loadBalanceHosts=true
 	// Здесь, судя по логике, должен быть второй сервер
-        String url2 = "jdbc:postgresql://localhost:5002/test?targetServerType=preferSecondary&loadBalanceHosts=true";
-        String user2 = "root";
-        String password2 = "";
+        String url2 = "jdbc:postgresql://localhost:5432/test?targetServerType=preferSecondary&loadBalanceHosts=true";
+        String user2 = "test";
+        String password2 = "password";
 
 	Connection con = null, con2 = null;
 	Boolean flgErr = false;
